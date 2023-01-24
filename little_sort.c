ac44562e261dbd36638dd2b618a8be1d1c323c35
Julien Laiti <julien.laiti@gmail.com>
2023-01-24 17:56:27 +0100

    maj sort 5

--- little_sort.c
+++ little_sort.c
@@ -6,7 +6,7 @@
 /*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 13:32:27 by jlaiti            #+#    #+#             */
-/*   Updated: 2023/01/24 15:33:01 by jlaiti           ###   ########.fr       */
+/*   Updated: 2023/01/24 17:30:31 by jlaiti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	little_sort_3(t_stack *a)
 		ra(a);
 }
 
-int	find_index(t_stack *a, int to_find)
+static int	find_index(t_stack *a, int to_find)
 {
 	int	i;
 
@@ -56,14 +56,14 @@ int	find_index(t_stack *a, int to_find)
 	ft_error(__func__);
 	return (-1);
 }
-
-void	move_to_b(int index, int size, int *pos_1)
+/*
+static void	move_to_b(int index, int size, int *pos_1, t_stack *a)
 {
 	if (index >= 2)
 	{
 		while (index-- != -1)
 		{
-			ft_printf("ra\n");
+			//ft_printf("ra\n");
 			*pos_1 -= 1;
 		}
 	}
@@ -71,11 +71,62 @@ void	move_to_b(int index, int size, int *pos_1)
 	{
 		while (index++ < size)
 		{
-			ft_printf("rra\n");
+			//ft_printf("rra\n");
 			*pos_1 += 1;
 		}
 	}
 	ft_printf("pb\n");
+}
+*/
+
+static void	find_position_zero(int index, t_stack *a, t_stack *b)
+{
+	if (index == 0)
+		pb(&a, &b);
+	else if (index == 1)
+	{
+		sa(a);
+		pb(&a, &b);
+	}
+	else if (index == 2)
+	{
+		rra(a);
+		sa(a);
+		pb(&a, &b);
+	}
+	else if (index == 3)
+	{	
+		ra(a);
+		ra(a);
+		pb(&a, &b);
+	}
+	else if (index == 4)
+	{
+		ra(a);
+		pb(&a, &b);
+	}
+}
+
+static void	find_position_one(int index, t_stack *a, t_stack *b)
+{
+	if (index == 0)
+		pb(&a, &b);
+	else if (index == 1)
+	{
+		sa(a);
+		pb(&a, &b);
+	}
+	else if (index == 2)
+	{
+		ra(a);
+		ra(a);
+		pb(&a, &b);
+	}
+	else if (index == 3)
+	{
+		ra(a);
+		pb(&a, &b);
+	}
 }
 
 void	little_sort_4_5(t_stack *a, t_stack *b, int size)
@@ -83,10 +134,22 @@ void	little_sort_4_5(t_stack *a, t_stack *b, int size)
 	int	pos_0;
 	int	pos_1;
 
+	if (is_sorted(a))
+		return ;
 	pos_0 = find_index(a, 0);
-	pos_1 = find_index(a, 1);
-	move_to_b(pos_0, size, &pos_1);
-	move_to_b(pos_1, size, &pos_1);
+	//printf("pos_0->%d\n", pos_0);
+	if (size == 5)
+		pos_1 = find_index(a, 1);
+	//printf("pos_1->%d", pos_1);
+	find_position_zero(pos_0, a, b);
+	if (size == 5)
+		find_position_one(pos_1, a, b);
 	little_sort_3(a);
-	little_sort_3(b);
+	pa(&a, &b);
+	if (size == 5)
+		pa(&a, &b);
+	//move_to_b(pos_0, size, &pos_1);
+	//move_to_b(pos_1, size, &pos_1);
+	//little_sort_3(a);
+	//little_sort_3(b);
 }
